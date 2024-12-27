665cecfcc08f7a4448c1b3edbf369b81a015b310
yu <sun_eric@outlook.com>
2024-12-27 13:34:28 -0500

    minor update for wl

--- clipboard.py
+++ clipboard.py
@@ -1,25 +1,23 @@
 import subprocess
 
+
 def copy(string, target=None):
     extra_args = []
-    if target != None:
-        extra_args += ['-target', target]
+    if target is not None:
+        extra_args += ["-target", target]
 
     return subprocess.run(
-        ['xclip', '-selection', 'c'] + extra_args,
-        universal_newlines=True,
-        input=string
+        ["wl-copy"] + extra_args, universal_newlines=True, input=string
     )
+
 
 def get(target=None):
     extra_args = []
-    if target != None:
-        extra_args += ['-target', target]
+    if target is not None:
+        extra_args += ["-target", target]
 
     result = subprocess.run(
-        ['xclip', '-selection', 'c', '-o'] + extra_args,
-        stdout=subprocess.PIPE,
-        universal_newlines=True
+        ["wl-paste"] + extra_args, stdout=subprocess.PIPE, universal_newlines=True
     )
 
     # returncode = result.returncode
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import threading
 import Xlib
 from Xlib.display import Display
@@ -6,14 +7,15 @@ from Xlib.protocol import event
 
 from normal import normal_mode
 
-class Manager():
+
+class Manager:
     def __init__(self, inkscape_id):
         self.id = inkscape_id
         self.disp = Display()
         self.screen = self.disp.screen()
         self.root = self.screen.root
 
-        self.inkscape = self.disp.create_resource_object('window', inkscape_id)
+        self.inkscape = self.disp.create_resource_object("window", inkscape_id)
         self.mode = normal_mode
 
     def event(self, name, detail, state):
@@ -21,10 +23,14 @@ class Manager():
             time=X.CurrentTime,
             root=self.root,
             window=self.inkscape,
-            same_screen=0, child=Xlib.X.NONE,
-            root_x=0, root_y=0, event_x=0, event_y=0,
+            same_screen=0,
+            child=Xlib.X.NONE,
+            root_x=0,
+            root_y=0,
+            event_x=0,
+            event_y=0,
             state=state,
-            detail=detail
+            detail=detail,
         )
 
     def string_to_keycode(self, key):
@@ -34,18 +40,26 @@ class Manager():
 
     def press(self, key, mask=X.NONE):
         keycode = self.string_to_keycode(key)
-        self.inkscape.send_event(self.event(event.KeyPress, keycode, mask), propagate=True)
-        self.inkscape.send_event(self.event(event.KeyRelease, keycode, mask), propagate=True)
+        self.inkscape.send_event(
+            self.event(event.KeyPress, keycode, mask), propagate=True
+        )
+        self.inkscape.send_event(
+            self.event(event.KeyRelease, keycode, mask), propagate=True
+        )
         self.disp.flush()
         self.disp.sync()
 
     def grab(self):
-        self.inkscape.grab_key(X.AnyKey, X.AnyModifier, True, X.GrabModeAsync, X.GrabModeAsync)
+        self.inkscape.grab_key(
+            X.AnyKey, X.AnyModifier, True, X.GrabModeAsync, X.GrabModeAsync
+        )
 
         # Ungrab window manager shortcuts (Super + ...)
-        self.inkscape.ungrab_key(self.string_to_keycode('Super_L'), X.AnyModifier, True)
-        self.inkscape.ungrab_key(self.string_to_keycode('Alt_L'), X.AnyModifier, True)
-        self.inkscape.change_attributes(event_mask=X.KeyReleaseMask | X.KeyPressMask | X.StructureNotifyMask)
+        self.inkscape.ungrab_key(self.string_to_keycode("Super_L"), X.AnyModifier, True)
+        self.inkscape.ungrab_key(self.string_to_keycode("Alt_L"), X.AnyModifier, True)
+        self.inkscape.change_attributes(
+            event_mask=X.KeyReleaseMask | X.KeyPressMask | X.StructureNotifyMask
+        )
 
     def ungrab(self):
         self.inkscape.ungrab_key(X.AnyKey, X.AnyModifier, True)
@@ -72,8 +86,10 @@ def create(inkscape_id):
     m = Manager(inkscape_id)
     m.listen()
 
+
 def is_inkscape(window):
-    return (window.get_wm_class() and 'inkscape' in window.                        get_wm_class()[0])
+    return window.get_wm_class() and "inkscape" in window.get_wm_class()[0]
+
 
 def main():
     disp = Display()
@@ -83,10 +99,9 @@ def main():
     # First listen for existing windows
     for window in root.query_tree().children:
         if is_inkscape(window):
-            print('Found existing window')
+            print("Found existing window")
             listen = threading.Thread(target=create, args=[window.id])
             listen.start()
-
 
     # New windows
     root.change_attributes(event_mask=X.SubstructureNotifyMask)
@@ -96,12 +111,13 @@ def main():
             window = evt.window
             try:
                 if is_inkscape(window):
-                    print('New window!')
+                    print("New window!")
                     listen = threading.Thread(target=create, args=[window.id])
                     listen.start()
 
             except Xlib.error.BadWindow:
                 pass
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
